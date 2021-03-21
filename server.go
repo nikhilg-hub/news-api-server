b018081f7e60ff78b364097fbc9a814c3833cda9
Nikhil Gaigawali <nikhil.gaigawali@gmail.com>
2021-03-21 19:19:41 +0530

    Added category as a query param

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -33,9 +34,19 @@ func handler(c echo.Context) error {
 
 	country := c.QueryParam("country")
 	apiKey := c.QueryParam("apiKey")
+	category := c.QueryParam("category")
+	var urlBuilder strings.Builder
+	urlBuilder.WriteString("http://newsapi.org/v2/top-headlines?country=")
+	urlBuilder.WriteString(country)
+	urlBuilder.WriteString("&apiKey=")
+	urlBuilder.WriteString(apiKey)
+	if category != "" {
+		urlBuilder.WriteString("&category=")
+		urlBuilder.WriteString(category)
+	}
 
-	resp, err := http.Get("http://newsapi.org/v2/top-headlines?country=" +
-		country + "&apiKey=" + apiKey)
+	resp, err := http.Get(urlBuilder.String())
+	c.Response().Header().Set("Access-Control-Allow-Origin", "*")
 	if err != nil {
 		fmt.Println(err)
 		return c.String(http.StatusInternalServerError, "Error")
